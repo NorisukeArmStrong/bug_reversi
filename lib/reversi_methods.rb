c1114e3f3a074b6c1f58ba2b579809b8b0238535
norit <noritomo0424@yahoo.co.jp>
2024-02-24 16:39:25 +0900

    本来置けない位置に石を置けてしまうバグを修正

--- lib/reversi_methods.rb
+++ lib/reversi_methods.rb
@@ -51,8 +51,8 @@ module ReversiMethods
 
     turn_succeed = false
     Position::DIRECTIONS.each do |direction|
-      next_pos = pos.next_position(direction)
-      turn_succeed = true if turn(copied_board, next_pos, stone_color, direction)
+      target_pos = pos.next_position(direction)
+      turn_succeed = true if turn(copied_board, target_pos, stone_color, direction)
     end
 
     copy_board(board, copied_board) if !dry_run && turn_succeed
@@ -62,7 +62,7 @@ module ReversiMethods
 
   def turn(board, target_pos, attack_stone_color, direction)
     return false if target_pos.out_of_board?
-    return false if target_pos.stone_color(board) == attack_stone_color
+    return false if target_pos.stone_color(board) == attack_stone_color || target_pos.stone_color(board) == BLANK_CELL
 
     next_pos = target_pos.next_position(direction)
     if (next_pos.stone_color(board) == attack_stone_color) || turn(board, next_pos, attack_stone_color, direction)
@@ -81,7 +81,7 @@ module ReversiMethods
     board.each_with_index do |cols, row|
       cols.each_with_index do |cell, col|
         next unless cell == BLANK_CELL
-
+        # binding.b
         position = Position.new(row, col)
         return true if put_stone(board, position.to_cell_ref, attack_stone_color, dry_run: true)
       end
